61bda53ccfebae2eae3a803d653135e2648e7b77
casperOne <casperOne@caspershouse.com>
2019-01-13 17:14:57 -0500

    Removed RegenerateAsync method.

--- src/Ofl.Search.Abstractions/IIndex.cs
+++ src/Ofl.Search.Abstractions/IIndex.cs
@@ -8,8 +8,6 @@ namespace Ofl.Search
     {
         string Name { get; }
 
-        Task RegenerateAsync(CancellationToken cancellationToken);
-
         Task CreateAsync(CancellationToken cancellationToken);
 
         Task DestroyAsync(CancellationToken cancellationToken);
